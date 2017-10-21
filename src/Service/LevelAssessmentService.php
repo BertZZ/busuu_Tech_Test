9f4ef95c70ed7acedcc6640f52aa5239a1c9eb65
BertZZ <bertie.wooles@hotmail.co.uk>
2017-10-21 10:59:19 +0100

    Adds levels array and some logic

--- src/Service/LevelAssessmentService.php
+++ src/Service/LevelAssessmentService.php
@@ -8,8 +8,13 @@ class LevelAssessmentService implements LevelAssessmentServiceInterface
      * @param array $exercises
      * @return string|bool
      */
+    public $currentLevel = 0;
+    public $levelHistory = array();
+    public $levels = array("A1", "A2", "B1", "B2", "C1", "C2");
+
+
     public function calculateLevel(array $exercises)
     {
-        return false;
+        return $this->levels[$this->currentLevel];
     }
 }
